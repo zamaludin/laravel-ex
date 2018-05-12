58c9d4ef9c4f258b815ce66dc1123bfc8087ea1e
Zamaludin Abdulah <zamaludin@live.com>
2018-05-12 08:06:10 +0700

    5.3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5.3 meren</div>
             </div>
         </div>
     </body>
